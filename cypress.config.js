a7a358e0bed82379885d34ad02aaac4cae8f5498
Scott Prue <scott.prue@side.com>
2025-08-21 17:19:13 -0600

    fix: show typescript error

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,24 @@
+const webpackPreprocessor = require('@cypress/webpack-batteries-included-preprocessor')
+
+function getWebpackOptions () {
+  const options = webpackPreprocessor.getFullWebpackOptions()
+
+  options.resolve.fallback.crypto = require.resolve('crypto-browserify')
+
+  return options
+}
+
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on(
+        'file:preprocessor',
+        webpackPreprocessor({
+          // if using typescript, you will need to set the typescript option to true
+          typescript: true,
+          webpackOptions: getWebpackOptions(),
+        }),
+      );
+    },
   },
 }
